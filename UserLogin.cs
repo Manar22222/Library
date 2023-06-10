f124fa21e884c105683fb6928d8fc25598b27957
Manar22222 <132195909+Manar22222@users.noreply.github.com>
2023-06-10 06:53:56 +0300

    Add files via upload

---
+++ UserLogin.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Xml;
+
+namespace test
+{
+    public partial class UserLogin : Form
+    {
+        public UserLogin()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=MyLipraryDB;Integrated Security=TrueData Source=.;Initial Catalog=MyLipraryDB;Integrated Security=True;Connect Timeout=30");
+        private void bunifuButton9_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Really Quit?", "Exit", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+
+                Application.Exit();
+
+            }
+        }
+
+        private void bunifuButton1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("Select Count(*)From LibrarianTbl Where LibName='"+UnameTb.Text+"' and LibPassword='"+PasswordTb.Text+"'",con);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            if (dt.Rows[0][0].ToString()=="1")
+            {
+                this.Hide();
+                Main main = new Main();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Wrong UserName Or Password");
+            }
+            con.Close();
+        }
+
+        private void bunifuButton8_Click(object sender, EventArgs e)
+        {
+            UnameTb.Text = "";
+            PasswordTb.Text = "";
+        }
+
+        private void bunifuButton10_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+
+        }
+    }
+}
