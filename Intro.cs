142064601d04a9ff8d137e10db441d86d5a66015
Manar22222 <132195909+Manar22222@users.noreply.github.com>
2023-06-10 06:45:53 +0300

    Add files via upload

---
+++ Intro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Intro : Form
+    {
+        public Intro()
+        {
+            InitializeComponent();
+        }
+
+   
+
+        int startpoint = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            startpoint += 1;
+            bunifuProgressBar1.Value = startpoint;
+            bunifuLabel4.Text = ""+startpoint;
+
+            if (bunifuProgressBar1.Value==100)
+            {
+                bunifuProgressBar1.Value = 0;
+                timer1.Stop();
+                UserLogin log = new UserLogin();
+                log.Show();
+                this.Hide();
+            }
+
+        }
+
+        private void Intro_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
