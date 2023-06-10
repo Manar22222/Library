bd8d0b2959c4d9b08c0e2608927521548fefd19c
Manar22222 <132195909+Manar22222@users.noreply.github.com>
2023-06-10 06:41:53 +0300

    Add files via upload

---
+++ AboutUs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class AboutUs : Form
+    {
+        public AboutUs()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuButton9_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/FattemaAshraf");
+        }
+
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/amna4499");
+
+        }
+    }
+}
